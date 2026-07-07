b024c3bb9574b81ec0063808f53fd60bed86caf4
Camille Cash <camille.cash96@gmail.com>
2026-07-07 00:40:16 -0400

    Astro React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import {defineConfig} from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import cloudflare from '@astrojs/cloudflare'
+import react from '@astrojs/react'
 
 export default defineConfig({
   site: 'https://wcashdesign.com',
   adapter: cloudflare(),
-  integrations: [sitemap()],
+  integrations: [react(), sitemap()],
 })
